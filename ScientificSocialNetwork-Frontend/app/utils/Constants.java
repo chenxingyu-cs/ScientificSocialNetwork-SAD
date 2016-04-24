43b602fee7657d477005e01c9a209fd5b5ffadbd
Yuanchen Bai <yuanchenbai@gmail.com>
2016-04-24 08:24:46 -0700

    fix createPost.scala.html

--- ScientificSocialNetwork-Frontend/app/utils/Constants.java
+++ ScientificSocialNetwork-Frontend/app/utils/Constants.java
@@ -9,37 +9,22 @@ package utils;
  *
  */
 public class Constants {
-<<<<<<< HEAD
   // server
   public static final String URL_HOST = "http://localhost";
-=======
-	// server
-	public static final String URL_HOST = "http://localhost";
-	
-	// port
-	public static final String LOCAL_HOST_PORT = ":9068";
-	public static final String CMU_BACKEND_PORT = ":9069";
+
 	// forum
-	public static final String ADD_NEW_POST = "/forum/createPost";
->>>>>>> byc
 
   // port
   public static final String LOCAL_HOST_PORT = ":9068";
   public static final String CMU_BACKEND_PORT = ":9069";
   // forum
-  public static final String ADD_NEW_POST = " /forum/createPost";
+  public static final String ADD_NEW_POST = "/forum/createPost";
 
-<<<<<<< HEAD
   // API Call format
   public static final String FORMAT = "json";
-=======
 	// add all parameter
-	public static final String ADD_ALL_PARAMETERS = "/parameter/addParameter";
-	
-	public static final String GET_ALL_PUBLICATIONS = "/publication/getAllPublications/json";
-	public static final String GET_PUBLICATION_PANEL = "/publication/getPublicationPanel/";
+
 	public static final String GET_MOST_POPULAR_PUBLICATIONS = "/publication/getMostPopularPublications/json";
->>>>>>> byc
 
   // add all parameter
   public static final String ADD_ALL_PARAMETERS = "/parameter/addParameter";
